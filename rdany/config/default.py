0dd1f70806aa672965966f86210e7280ba9aeb3e
root <eibriel@eibriel.com>
2017-04-09 16:45:58 -0300

    Ignoring DB and local settings, adding parameters to default config

--- rdany/config/default.py
+++ rdany/config/default.py
@@ -2,3 +2,7 @@ class Config(object):
     BOT_USERNAME = "rDanyBot"
     PAGE_ACCESS_TOKEN = ""
     HUB_VERIFY_TOKEN = ""
+    TELEGRAM_TOKEN = ""
+    TELEGRAM_ADMIN_ID = 1234
+    KIK_TOKEN = ""
+    SIMSIMI_TOKEN = ""
